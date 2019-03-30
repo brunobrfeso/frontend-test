a97567150b5c68dc5ec6681cd0d6ec43bc17c8bf
Bruno Brfeso <brunobrfeso@gmail.com>
2019-03-30 11:18:13 -0300

    Updates

---
+++ precache-manifest.4efbe28bf9d2862f512cd9facdf365bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "29e5b6a2df242c2df158",
+    "url": "/frontend-test/static/css/main.097aaf72.chunk.css"
+  },
+  {
+    "revision": "29e5b6a2df242c2df158",
+    "url": "/frontend-test/static/js/main.70b65f0c.chunk.js"
+  },
+  {
+    "revision": "4c6d7678377177b84aaa",
+    "url": "/frontend-test/static/js/runtime~main.7b7e8cdc.js"
+  },
+  {
+    "revision": "3ca34cb5beec10b77325",
+    "url": "/frontend-test/static/js/2.87c59ccd.chunk.js"
+  },
+  {
+    "revision": "85dc57528bf74826bf21",
+    "url": "/frontend-test/static/js/3.b451e973.chunk.js"
+  },
+  {
+    "revision": "6ba49cbe58460a6d0ae0",
+    "url": "/frontend-test/static/js/4.2deec8d3.chunk.js"
+  },
+  {
+    "revision": "0d458171ab643f8a0ea2",
+    "url": "/frontend-test/static/js/5.04985430.chunk.js"
+  },
+  {
+    "revision": "266dab744a33da48d01e",
+    "url": "/frontend-test/static/js/6.c21e5b79.chunk.js"
+  },
+  {
+    "revision": "78f7da406dfa993bc72dae296d201db2",
+    "url": "/frontend-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87f25249f76cc1c3f8ada2d00e1e519f.js"
+  "/frontend-test/precache-manifest.4efbe28bf9d2862f512cd9facdf365bd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/frontend-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
